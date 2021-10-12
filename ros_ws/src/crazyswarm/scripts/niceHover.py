d6ba7547c794558cb1ca655137cd5f862aca366f
sumeet <sscifi@gmail.com>
2021-10-11 17:41:00 -0700

    ekf and libobjecttracker changes made with james

--- ros_ws/src/crazyswarm/scripts/niceHover.py
+++ ros_ws/src/crazyswarm/scripts/niceHover.py
@@ -3,7 +3,7 @@
 import numpy as np
 from pycrazyswarm import *
 
-Z = 1.0
+Z = 1.5
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
     timeHelper.sleep(1.5+Z)
     for cf in allcfs.crazyflies:
-        pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
-        cf.goTo(pos, 0, 1.0)
+        init_pos = np.array(cf.initialPosition)
+        pos = init_pos + np.array([0, 0, Z])
+        cf.goTo(np.array([0, 0, Z]), 0, 3.0)
 
     print("press button to continue...")
     swarm.input.waitUntilButtonPressed()
